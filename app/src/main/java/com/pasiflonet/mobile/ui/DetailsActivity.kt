d1be4d27b60b2e0456b8be6664528e668ddb2ccb
YOUR_NAME <YOUR_EMAIL>
2026-01-05 10:45:14 +0200

    Hotfix: fix SendWorker WorkRequest addTag corruption (single SEND_WORK tag)

--- app/src/main/java/com/pasiflonet/mobile/ui/DetailsActivity.kt
+++ app/src/main/java/com/pasiflonet/mobile/ui/DetailsActivity.kt
@@ -348,7 +348,7 @@ setContentView(R.layout.activity_details)
             .putFloat(SendWorker.KEY_WM_Y, wmY)
             .build()
 
-        val req = OneTimeWorkRequestBuilder<SendWorker>().addTag("SEND_WORK").addTag("SEND_WORK").addTag("SEND_WORK").addTag("SEND_WORK").addTag(com.pasiflonet.mobile.worker.SendWorker"SEND").addTag("SEND_WORK").addTag("SEND_WORK").addTag("SEND_WORK").addTag("SEND_WORK").addTag("SEND_WORK")
+        val req = OneTimeWorkRequestBuilder<SendWorker>().addTag("SEND_WORK")
             .setInputData(data)
             .build()
 
